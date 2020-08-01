2e05aa10b57541d8f2a18120351077a49d72af51
skiym-aaa <y.sakiyama3216@gmail.com>
2020-08-01 05:12:48 +0000

    [Add] スクレイピング

--- scrape.rb
+++ scrape.rb
@@ -1,25 +1,24 @@
-require 'net/http'
+require 'nokogiri'
+require 'json'
 
-# def get_from(url)
-#   url = 'https://masayuki14.github.io/pit-news/'
-#   uri = URI(url)
-#   html = Net::HTTP.get(uri)
-#   # htmlを返す
-#   return 'html'
-# end
+html = File.open('pitnews.html', 'r') {|f| f.read}
+doc = Nokogiri::HTML.parse(html,nil,'utf-8')
 
-def get_from(url)
-  Net::HTTP.get(URI(url))
+pitnews = []
+doc.xpath('/html/body/main/section').each_with_index do |section, index|
+  next if index.zero?
+  contents = {category: nil, news:[]}
+  contents[:category] = section.xpath('./h6').first.text
+
+  section.xpath('./div/div').each do |node|
+    title = node.xpath('./p/strong/a').first.text
+    url = node.xpath('./p/strong/a').first['href']
+
+    news = {title: title, url: url}
+    contents[:news] << news
+  end
+
+  pitnews << contents
 end
 
-# def write_file(path, text)
-#   file = File.open(path, 'w')
-#   file.write(text)
-#   file.close
-# end
-
-def write_file(path, text)
-  File.open(path, 'w') { |file| file.write(text) }
-end
-
-write_file('pitnews.html', get_from('https://masayuki14.github.io/pit-news/'))
+File.open('pitnews.json','w') { |file| file.write({pitnews: pitnews}.to_json)}
