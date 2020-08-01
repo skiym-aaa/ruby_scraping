b3b4bcc419425d3e6bfbdced1e5427cf97c552ae
skiym-aaa <y.sakiyama3216@gmail.com>
2020-08-01 06:54:55 +0000

    スクレイピング/リフファクタリング

--- scrape.rb
+++ scrape.rb
@@ -1,24 +1,66 @@
+require 'net/http'
 require 'nokogiri'
 require 'json'
 
-html = File.open('pitnews.html', 'r') {|f| f.read}
-doc = Nokogiri::HTML.parse(html,nil,'utf-8')
+# def get_from(url)
+#   url = 'https://masayuki14.github.io/pit-news/'
+#   uri = URI(url)
+#   html = Net::HTTP.get(uri)
+#   # htmlを返す
+#   return 'html'
+# end
 
-pitnews = []
-doc.xpath('/html/body/main/section').each_with_index do |section, index|
-  next if index.zero?
-  contents = {category: nil, news:[]}
-  contents[:category] = section.xpath('./h6').first.text
-
-  section.xpath('./div/div').each do |node|
-    title = node.xpath('./p/strong/a').first.text
-    url = node.xpath('./p/strong/a').first['href']
-
-    news = {title: title, url: url}
-    contents[:news] << news
-  end
-
-  pitnews << contents
+def get_from(url)
+  Net::HTTP.get(URI(url))
 end
 
-File.open('pitnews.json','w') { |file| file.write({pitnews: pitnews}.to_json)}
+# def write_file(path, text)
+#   file = File.open(path, 'w')
+#   file.write(text)
+#   file.close
+# end
+
+def write_file(path, text)
+  File.open(path, 'w') { |file| file.write(text) }
+end
+
+# html = File.open('pitnews.html', 'r') {|f| f.read}
+# doc = Nokogiri::HTML.parse(html,nil,'utf-8')
+
+# pitnews = []
+# doc.xpath('/html/body/main/section[position() > 1]').each_with_index do |section, index|
+#   next if index.zero?
+#   contents = {category: nil, news:[]}
+#   contents[:category] = section.xpath('./h6').first.text
+
+#   section.xpath('./div/div').each do |node|
+#     title = node.xpath('./p/strong/a').first.text
+#     url = node.xpath('./p/strong/a').first['href']
+
+#     news = {title: title, url: url}
+#     contents[:news] << news
+#   end
+
+#   pitnews << contents
+# end
+
+# File.open('pitnews.json','w') { |file| file.write({pitnews: pitnews}.to_json)}
+
+def scrape_news(news)
+  {
+    title: news.xpath('./p/strong/a').first.text,
+    url: news.xpath('./p/strong/a').first['href']
+  }
+end
+
+def scrape_section(section)
+  {
+    category: section.xpath('./h6').first.text,
+    news:  section.xpath('./div/div').map { |node| scrape_news(node) }
+  }
+end
+
+html = File.read('pitnews.html')
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+pitnews = doc.xpath('/html/body/main/section[position() > 1]').map { |section| scrape_section(section) }
+write_file('pitnews.json', {pitnews: pitnews}.to_json)
